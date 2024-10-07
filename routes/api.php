2d2d36f7bd6bceae7e4e22a6bc56018adb452367
edi-prasetyo <ediprasetiyo2@gmail.com>
2024-10-07 19:09:09 +0700

    Initial Commit

--- routes/api.php
+++ routes/api.php
@@ -10,14 +10,13 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 // Public Routes
-
+Route::post('/login', [AuthController::class, 'login']);
 // Route::post('/register', [AuthController::class, 'register']);
 
 
 // Protected Routes
-Route::group(['prefix' => 'v1', 'middleware' => ['auth:sanctum']], function () {
+Route::group(['middleware' => ['auth:sanctum']], function () {
 
-    Route::post('/login', [AuthController::class, 'login']);
     Route::controller(AuthController::class)->group(function () {
         Route::post('/logout', 'logout');
         Route::get('/profile', 'profile');
